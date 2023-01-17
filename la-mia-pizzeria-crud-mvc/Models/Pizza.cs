db8d299ae092fbb8022201de4c9f7c1795516073
Valerio Federico Colombo <colombovf@gmail.com>
2023-01-17 15:20:49 +0100

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace la_mia_pizzeria_crud_mvc.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
