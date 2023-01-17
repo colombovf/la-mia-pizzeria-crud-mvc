20cefdb428d923d939f974d7161c3cdaf0782393
Valerio Federico Colombo <colombovf@gmail.com>
2023-01-17 15:24:59 +0100

    Aggiunto model Pizza e pacchetti NuGet

--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -1,10 +1,16 @@
-﻿namespace la_mia_pizzeria_crud_mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_crud_mvc.Models
 {
     public class Pizza
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Image { get; set; }
     }
 }
